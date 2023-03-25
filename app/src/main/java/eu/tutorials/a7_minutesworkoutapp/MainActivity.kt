96b4eae1ac502f69fef487203837f1db15d5084b
bjl0615 <bjl0615@gmail.com>
2023-03-26 00:16:57 +0900

    ViewBinding and Action_Bar Delete

--- app/src/main/java/eu/tutorials/a7_minutesworkoutapp/MainActivity.kt
+++ app/src/main/java/eu/tutorials/a7_minutesworkoutapp/MainActivity.kt
@@ -1,22 +1,36 @@
 package eu.tutorials.a7_minutesworkoutapp
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
 import android.widget.Toast
+import eu.tutorials.a7_minutesworkoutapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding : ActivityMainBinding? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
 
-        val fLStartButton:FrameLayout = findViewById(R.id.flStart)
-        fLStartButton.setOnClickListener {
+
+//        val fLStartButton:FrameLayout = findViewById(R.id.flStart)
+        binding?.flStart?.setOnClickListener {
             Toast.makeText(
                 this@MainActivity,
                 "Here we will start the exercise.",
                 Toast.LENGTH_SHORT
             ).show()
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        binding = null
     }
 }
